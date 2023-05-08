0d93c7ac6180e4c007f7c81ed804040d3eb2bf62
Jesús Moncada Ramírez <jesus.moncadaramirez@studenti.unipd.it>
2023-05-08 15:57:44 +0200

    refactored document parsing

--- code/src/main/java/parse/LongEvalParser.java
+++ code/src/main/java/parse/LongEvalParser.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.Reader;
 
 /**
- * Implements a document parser for the LongEval corpus (JSON version).
+ * Implements a stream document parser for the LongEval corpus (JSON version).
  *
  * @version 1.00
  * @since 1.00
@@ -17,26 +17,22 @@ import java.io.Reader;
 public class LongEvalParser extends DocumentParser {
 
     /**
-     * The size of the buffer for the body element.
-     */
-    private static final int BODY_SIZE = 1024 * 8;
-
-    /**
      * The currently parsed document.
      */
     private ParsedDocument document = null;
 
     /**
-     * The JSON reader to be used to parse document(s). This JsonReader will be used instead of the Reader in.
+     * The JSON reader to be used to parse document(s).
+     * We will use this object (JsonReader) instead of Reader in.
      */
-    private JsonReader in_json;
+    private final JsonReader in_json;
 
     /**
-     * Creates a new Longeval Corpus (JSON) document parser.
+     * Creates a new Longeval Corpus (JSON) stream document parser.
      *
-     * @param in the reader to the document(s) to be parsed.
+     * @param in the reader of the document(s) to be parsed.
      * @throws NullPointerException     if {@code in} is {@code null}.
-     * @throws IllegalArgumentException if any error occurs while creating the parser.
+     * @throws IllegalArgumentException if any error initializing the JSON parser.
      */
     public LongEvalParser(final Reader in) {
         super(new BufferedReader(in));
@@ -54,8 +50,8 @@ public class LongEvalParser extends DocumentParser {
         // JSON stream reading taken from: https://www.amitph.com/java-parse-large-json-files/
         try {
             if (in_json.hasNext()) {
-                JsonDocument jdoc = new Gson().fromJson(in_json, JsonDocument.class);
-                document = new ParsedDocument(jdoc.getId(), jdoc.getContents());
+                JsonDocument jsdoc = new Gson().fromJson(in_json, JsonDocument.class);
+                document = new ParsedDocument(jsdoc.getId(), jsdoc.getContents());
                 next = true;
             } else {
                 in_json.endArray();
@@ -83,18 +79,17 @@ public class LongEvalParser extends DocumentParser {
      * @throws Exception if something goes wrong while indexing.
      */
     public static void main(String[] args) throws Exception {
-
-        final String FILE_NAME = "C:\\longeval_train\\publish\\English\\Documents\\Json\\collector_kodicare_1.txt.json";
+        final String FILE_NAME_JMR = "C:\\longeval_train\\publish\\English\\Documents\\Json\\collector_kodicare_1.txt.json";
         Reader reader = new FileReader(
-                FILE_NAME);
+                FILE_NAME_JMR);
 
         LongEvalParser p = new LongEvalParser(reader);
 
-        System.out.printf("Starting document parsing at %s %n", FILE_NAME);
+        System.out.printf("Starting document parsing at %s...%n", FILE_NAME_JMR);
         int count = 0;
         for (ParsedDocument d : p) {
-            //System.out.printf("%n%n------------------------------------%n%s%n%n%n", d.toString());
-            //System.out.printf("Parsed document with id=%s %n", d.getIdentifier());
+            System.out.printf("%n%n------------------------------------%n%s%n%n%n", d.toString());
+            System.out.printf("Parsed document with id=%s %n", d.getIdentifier());
             count++;
         }
         System.out.printf("Number of parsed documents: %d", count);
